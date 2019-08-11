d0168c01a06511a4a7914fe6b14d07282bc37b5c
李文浩 <liwenhao@liwenhaodeMac-mini.local>
2019-08-11 14:19:17 +0800

    superadmin

--- src/store/index.js
+++ src/store/index.js
@@ -16,7 +16,11 @@ export const store =new Vuex.Store({
         user: null,
         status:null,
         roles:['/dashboard','/console','/EduAdmin','/Admin','/superAdmin'],
-        roleId:0
+        roleId:0,
+        //superAdmin data
+        adminList:[],
+        adminCurrentList:[],
+        limit:10
     },
     getters:{
         isAuthenticated(state) {
@@ -37,6 +41,13 @@ export const store =new Vuex.Store({
           clearAuthData(state) {
             state.idToken = null;
             state.userId = null;
+          },
+          changeAdminList(state,adminList){
+            state.adminList=adminList
+            
+          },
+          changeAdminCurrentList(state,value){
+            state.adminCurrentList=state.adminList.slice(value, value + state.limit);
           }
     },
     actions:{
@@ -69,8 +80,46 @@ export const store =new Vuex.Store({
         localStorage.removeItem('userId');
         router.replace('/')
         // localStorage.removeItem('userId');
-      }
-        
-    },
-    
+      },
+      //superAdmin方法
+      //superAdmin get Admin 
+      
+      getAdmin({commit,dispatch,state}){
+        globalAxios.get(
+        "/superadmin/admin",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: state.idToken
+          }
+        }
+      )
+      .then(
+        response=>{
+          console.log(response)
+          //console.log(response.data.data);
+          var admin_arr = response.data.data;
+          var admin_table = [];
+          for (var i = 0; i < admin_arr.length; i++) {
+            var admin = {};
+            admin.username = admin_arr[i].USER_NAME;
+            admin.id = admin_arr[i].USER_ID;
+            if (admin_arr[i].USER_STATUS == "active") admin.status = "启用";
+            else admin.status = "禁用";
+            admin.character = "管理员";
+            //console.log(admin)
+            admin_table.push(admin);
+          }
+          //console.log(admin_table)
+          commit("changeAdminList",admin_table)
+          commit("changeAdminCurrentList",0)
+
+      },
+        error=>{
+          console.log(error)
+
+        }
+      )
+    }
+  }
 })
