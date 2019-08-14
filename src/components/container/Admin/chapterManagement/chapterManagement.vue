a89b684ca0ae48cd8b27dea477dedc3adfa9d92b
李文浩 <liwenhao@liwenhaodeMac-mini.local>
2019-08-14 15:58:11 +0800

    解决冲突

--- src/components/container/Admin/chapterManagement/chapterManagement.vue
+++ src/components/container/Admin/chapterManagement/chapterManagement.vue
@@ -102,13 +102,9 @@
             changeTablePages(value) {
                 this.currentList = this.videoData.slice(value, value + this.limit);
             },
-<<<<<<< HEAD
-            gotoAddChapter(){
-                this.$router.push({ path: '/Admin/chapterManagement/addChapter' })
-=======
+
             gotoUpload(){
                 this.$router.push({ path: '/Admin/chapterManagement/'+this.courseName+'/addChapter' })
->>>>>>> d22d9b22a26ab5d06951da5565d28aa5dd17bba8
             },
         },
         mounted() {
