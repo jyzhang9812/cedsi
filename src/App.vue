561cfd08f251f84feac9bfefc5410f523d3fee30
Huiquan Zhang <huiquanzhang@HuiquandeMacBook-Pro.local>
2019-10-11 16:29:24 +0800

    解决了切换页面高度变短的问题

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app" :style="appheight">
-    <app-header/>
-    <vue-element-loading :active="vueElementLoading" :is-full-screen="true"
-    spinner="bar-fade-scale" color="#FF6700" />
+    <app-header />
+    <vue-element-loading :active="vueElementLoading" :is-full-screen="true" spinner="bar-fade-scale" color="#FF6700" />
     <router-view :style="screenHeight"></router-view>
-    <app-footer/>
+    <app-footer />
   </div>
 </template>
 
 <script>
   import Header from './components/header/header.vue'
   import Footer from './components/footer/footer.vue'
-  import VueElementLoading from'vue-element-loading'
+  import VueElementLoading from 'vue-element-loading'
   import { mapState } from 'vuex'
 
   export default {
@@ -25,26 +24,33 @@
       return {
         height: 0,
         width: 0,
-        appheight:0,
+        appheight: 0,
       }
     },
     created: function () {
       this.height = document.documentElement.clientHeight,
-      console.log(this.height)
-      this.appheight = 'min-height:' + this.height + 'px;';
-      this.screenHeight = 'min-height:' + (this.height - 56) + 'px;';
+        console.log(this.height)
+      if (this.height > 620) {
+        this.appheight = 'min-height:' + this.height + 'px;';
+        this.screenHeight = 'min-height:' + (this.height - 56) + 'px;';
+      } else {
+        this.appheight = 'min-height:650px;';
+        this.screenHeight = 'min-height:' + (this.height - 56) + 'px;';
+      }
       this.$store.dispatch('tryAutoLogin');
     },
-    computed:{
-    ...mapState({  //...对象扩展符
-      vueElementLoading:'vueElementLoading'
-    })
+    computed: {
+      ...mapState({  //...对象扩展符
+        vueElementLoading: 'vueElementLoading'
+      })
+    }
   }
-}
 </script>
 
 <style>
-  body, html, #app {
+  body,
+  html,
+  #app {
     margin: 0;
     min-height: 100%;
     /* font-family: "Helvetica Neue", Helvetica, Arial, sans-serif; */
