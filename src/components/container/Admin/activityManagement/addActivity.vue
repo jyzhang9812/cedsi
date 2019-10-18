6ce087d68082a6e8e2b65856b4fb4cdde1ab31ef
rumosky <rumosky@163.com>
2019-10-19 00:42:18 +0800

    管理员发布活动彻底完成

--- src/components/container/Admin/activityManagement/addActivity.vue
+++ src/components/container/Admin/activityManagement/addActivity.vue
@@ -27,20 +27,20 @@
     <div class="upload upload-height">
       <span class="upload-title">预览:</span>
       <div class="upload-cover-img">
-        <img id="headimage" :src="coverImage" class="cover-image" alt v-show="coverImage!==''" />
+        <img id="headimage" :src="coverImageDisplay" class="cover-image" />
       </div>
     </div>
     <div class="upload">
       <span class="upload-title">上传内容图片:</span>
       <div class="upload-cover-btn">
         上传图片
-        <input type="file" @change="getActivityImg($event)" style="opacity: 0" multiple="multiple" />
+        <input type="file" @change="getActivityImg($event)" style="opacity: 0" />
       </div>
     </div>
     <div class="upload upload-height">
       <span class="upload-title">预览:</span>
       <div class="upload-cover-img">
-        <img id="headimage" :src="coverImages[0]" class="cover-image" alt v-show="coverImages[0]!==''" />
+        <img id="headimage" :src="activityContentImageDisplay" class="cover-image" />
       </div>
     </div>
     <div class="upload-footer">
@@ -75,12 +75,12 @@
         activityName: "",
         activityPrice: "",
         coverImage: "",
-        fileName: "",
+        coverImageDisplay: "",
+        coverType: "",
+        activityContentImage: "",
+        activityContentImageDisplay: "",
+        activityContentImageType: "",
         activityAddress: "",
-        activityIntro: "",
-        cover: [],
-        coverImages: [],
-        coverFile: []
       };
     },
     methods: {
@@ -95,61 +95,47 @@
         });
       },
       getCoverFile(event) {
-        this.licenseFile = event.target.files[0];
-        console.log(this.licenseFile);
-        this.licenseFileName = this.licenseFile.name;
-        this.licenseType = this.licenseFile.type.split("/")[1];
-        this.licenseSize = this.licenseFile.size;
-        var reader = new FileReader();
-        var that = this;
-        reader.readAsDataURL(this.licenseFile);
+        this.coverImage = event.target.files[0];
+        console.log(this.coverImage);
+        this.coverType = '.' + this.coverImage.type.split("/")[1];
+        let reader = new FileReader();
+        let that = this;
+        reader.readAsDataURL(this.coverImage);
         reader.onload = function (e) {
-          that.coverImage = this.result;
+          that.coverImageDisplay = this.result;
         };
       },
       getActivityImg(event) {
-        for (var i = 0; i < event.target.files.length; i++) {
-          let that = this;
-          let file = event.target.files[i];
-          let reader = new FileReader();
-          // 调用reader.readAsDataURL()方法，把图片转成base64
-          reader.readAsDataURL(file);
-          // 监听reader对象的onload事件，当图片加载完成时，把base64编码賦值给预览图片
-          reader.onload = function () {
-            file.src = this.result;
-            // console.log(this); 这里的this是FileReader对象
-            // console.log(file)
-            // 再把file对象添加到img数组
-            //console.log(file.src);
-            that.coverImages.push(file.src);
-          };
-        }
+        this.activityContentImage = event.target.files[0];
+        console.log(this.activityContentImage);
+        this.activityContentImageType = '.' + this.activityContentImage.type.split("/")[1];
+        let reader = new FileReader();
+        let that = this;
+        reader.readAsDataURL(this.activityContentImage);
+        reader.onload = function (e) {
+          that.activityContentImageDisplay = this.result;
+        };
       },
       cancel() {
         this.$router.replace({
           path: "/Admin/activityManagement/"
         });
       },
-      uploadFourImages(config) {
+      uploadTwoImages(config) {
         let missions = [];
-        let files = [this.licenseFile].concat(this.coverFiles);
         let rawId = config.id;
-        for (let i = 1; i < 4; i += 1) {
-          config.id = `${rawId}${i}`;
-          missions.push(this.uploadToBucket(config, files[i - 1]));
-        }
+        config.id = `${rawId}1`;
+        missions.push(this.uploadToBucket(config, this.coverImage));
+        config.id = `${rawId}2`;
+        missions.push(this.uploadToBucket(config, this.activityContentImage));
         return Promise.all(missions);
       },
       submit() {
-        let types = [this.licenseFile.type.split('/')[1] || 'null'];
-        for (let i = 0; i < 3; i += 1) {
-          let file = this.coverFiles[i];
-          types.push(file ? `.${file.type.split('/')[1]}` : 'null');
-        }
+        let types = [this.coverType, this.activityContentImageType]
         this.insertActivity(types)
           .then(res => {
             console.log(res);
-            return this.uploadFourImages(res.data.data);
+            return this.uploadTwoImages(res.data.data);
           })
           .then(res => {
             console.log(res);
@@ -157,8 +143,7 @@
           .catch(err => {
             console.log(err);
           })
-          this.$router.replace({ path: "/Admin/activityManagement/" });
-          $(window).scrollTop(0);
+        this.$router.replace({ path: "/Admin/activityManagement/" });
       },
       uploadToBucket(config, file) {
         AWS.config = new AWS.Config({
@@ -172,7 +157,7 @@
           ACL: 'public-read',
           Bucket: "cedsi",
           Body: file,
-          Key: "adminActivity/" + config.id + "." + file.type.split('/')[1],
+          Key: "activity/" + config.id + "." + file.type.split('/')[1],
           ContentType: file.type,
           Metadata: {
             'uploader': window.localStorage.getItem('user')
@@ -187,12 +172,12 @@
       insertActivity(types) {
         let config = { headers: { Authorization: localStorage.getItem('idToken') } };
         let data = {
-          "ACTIVITY_TIME": this.startDate,
-          "ACTIVITY_PLACE": this.activityAddress,
-          "ACTIVITY_TITLE": this.activityName,
-          "ACTIVITY_PRICE": this.activityPrice,
-          "COVER_TYPE": types[0],
-          "IMG_TYPE": types[1],
+          "activityTime": this.startDate,
+          "activityPlace": this.activityAddress,
+          "activityTitle": this.activityName,
+          "activityPrice": this.activityPrice,
+          "coverType": types[0],
+          "imgType": types[1],
         }
         console.log(data);
         return instance.post("/admin/activity", data, config);
@@ -216,151 +201,179 @@
 </script>
 
 <style>
-#add-activity {
-  width: 98%;
-  margin: 0 auto;
-  padding-top: 30px;
-}
-#add-activity .upload {
-  width: 100%;
-  height: 50px;
-  margin-bottom: 20px;
-}
-#add-activity .upload-title {
-  color: #606266;
-  display: block;
-  text-align: right;
-  width: 100px;
-  height: 40px;
-  float: left;
-  line-height: 40px;
-}
-#add-activity .upload-input {
-  width: 300px;
-  height: 40px;
-  border-radius: 5px;
-  border: 1px solid #409eff;
-  margin-left: 10px;
-  padding-left: 10px;
-}
-#add-activity .upload-input:hover {
-  border: 1px solid #66b1ff;
-}
-#add-activity .upload-input:focus {
-  outline: none;
-}
-#add-activity .activity-time {
-  margin-left: 10px !important;
-  height: 40px !important;
-  width: 300px !important;
-}
-#add-activity .upload-textarea {
-  border: 1px solid #409eff;
-  border-radius: 5px;
-  margin-left: 10px;
-  padding: 10px;
-}
-#add-activity .upload-height {
-  height: 190px;
-}
-#add-activity .upload-textarea:hover {
-  border: 1px solid #66b1ff;
-}
-#add-activity .upload-textarea:focus {
-  outline: none;
-}
-#add-activity .outside {
-  width: 300px !important;
-  height: 40px !important;
-  margin-left: 10px !important;
-}
-#add-activity .inputBox,
-#add-activity .inputbox {
-  height: 35px !important;
-  font-size: 14px !important;
-  width: 230px !important;
-}
+  #add-activity {
+    width: 98%;
+    margin: 0 auto;
+    padding-top: 30px;
+  }
 
-#add-activity .dropdown-menu {
-  left: 100px !important;
-}
-#add-activity .upload-footer {
-  width: 100%;
-  text-align: center;
-  margin-bottom: 20px;
-}
-#add-activity .upload-btn {
-  background-color: #409eff;
-  color: #fff;
-  margin-left: 10px;
-}
-#add-activity .upload-btn:hover {
-  color: #fff;
-}
-#add-activity .upload-btn:focus {
-  outline: none;
-  color: #fff;
-}
-/**/
-#add-activity .address-input select {
-  margin-left: 10px;
-  font-size: 14px;
-}
-#add-activity .upload-cover-btn {
-  margin-left: 10px;
-  width: 80px;
-  height: 35px;
-  display: inline-block;
-  background-color: #409eff;
-  color: #fff;
-  border-radius: 5px;
-  line-height: 35px;
-  text-align: center;
-}
-#add-activity input[type="file"] {
-  width: 80px;
-  height: 35px;
-  position: relative;
-  top: -35px;
-}
-#add-activity .upload-cover-img {
-  display: inline-block;
-  border: 1px dashed #dcdfe6;
-  width: 290px;
-  height: 150px;
-  margin-left: 10px;
-  border-radius: 5px;
-  background-color: #f5f7fa;
-}
-#add-activity .cover-image {
-  width: 100%;
-  height: 100%;
-}
-#add-activity .upload-height {
-  height: 190px;
-}
-#add-activity .editor {
-  width: 800px;
-  position: relative;
-}
-#add-activity .w-e-toolbar {
-  position: relative;
-  left: 10px;
-}
-#add-activity .w-e-text-container {
-  position: relative;
-  left: 110px;
-  width: 700px;
-}
-#add-activity .w-e-text {
-  position: relative;
-  top: -10px;
-  background-color: #fff;
-}
-#add-activity .upload-height2 {
-  height: 350px;
-}
-#add-activity .upload-width {
-  width: 200px;
-}
+  #add-activity .upload {
+    width: 100%;
+    height: 50px;
+    margin-bottom: 20px;
+  }
+
+  #add-activity .upload-title {
+    color: #606266;
+    display: block;
+    text-align: right;
+    width: 100px;
+    height: 40px;
+    float: left;
+    line-height: 40px;
+  }
+
+  #add-activity .upload-input {
+    width: 300px;
+    height: 40px;
+    border-radius: 5px;
+    border: 1px solid #409eff;
+    margin-left: 10px;
+    padding-left: 10px;
+  }
+
+  #add-activity .upload-input:hover {
+    border: 1px solid #66b1ff;
+  }
+
+  #add-activity .upload-input:focus {
+    outline: none;
+  }
+
+  #add-activity .activity-time {
+    margin-left: 10px !important;
+    height: 40px !important;
+    width: 300px !important;
+  }
+
+  #add-activity .upload-textarea {
+    border: 1px solid #409eff;
+    border-radius: 5px;
+    margin-left: 10px;
+    padding: 10px;
+  }
+
+  #add-activity .upload-height {
+    height: 190px;
+  }
+
+  #add-activity .upload-textarea:hover {
+    border: 1px solid #66b1ff;
+  }
+
+  #add-activity .upload-textarea:focus {
+    outline: none;
+  }
+
+  #add-activity .outside {
+    width: 300px !important;
+    height: 40px !important;
+    margin-left: 10px !important;
+  }
+
+  #add-activity .inputBox,
+  #add-activity .inputbox {
+    height: 35px !important;
+    font-size: 14px !important;
+    width: 230px !important;
+  }
+
+  #add-activity .dropdown-menu {
+    left: 100px !important;
+  }
+
+  #add-activity .upload-footer {
+    width: 100%;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  #add-activity .upload-btn {
+    background-color: #409eff;
+    color: #fff;
+    margin-left: 10px;
+  }
+
+  #add-activity .upload-btn:hover {
+    color: #fff;
+  }
+
+  #add-activity .upload-btn:focus {
+    outline: none;
+    color: #fff;
+  }
+
+  /**/
+  #add-activity .address-input select {
+    margin-left: 10px;
+    font-size: 14px;
+  }
+
+  #add-activity .upload-cover-btn {
+    margin-left: 10px;
+    width: 80px;
+    height: 35px;
+    display: inline-block;
+    background-color: #409eff;
+    color: #fff;
+    border-radius: 5px;
+    line-height: 35px;
+    text-align: center;
+  }
+
+  #add-activity input[type="file"] {
+    width: 80px;
+    height: 35px;
+    position: relative;
+    top: -35px;
+  }
+
+  #add-activity .upload-cover-img {
+    display: inline-block;
+    border: 1px dashed #dcdfe6;
+    width: 290px;
+    height: 150px;
+    margin-left: 10px;
+    border-radius: 5px;
+    background-color: #f5f7fa;
+  }
+
+  #add-activity .cover-image {
+    width: 100%;
+    height: 100%;
+  }
+
+  #add-activity .upload-height {
+    height: 190px;
+  }
+
+  #add-activity .editor {
+    width: 800px;
+    position: relative;
+  }
+
+  #add-activity .w-e-toolbar {
+    position: relative;
+    left: 10px;
+  }
+
+  #add-activity .w-e-text-container {
+    position: relative;
+    left: 110px;
+    width: 700px;
+  }
+
+  #add-activity .w-e-text {
+    position: relative;
+    top: -10px;
+    background-color: #fff;
+  }
+
+  #add-activity .upload-height2 {
+    height: 350px;
+  }
+
+  #add-activity .upload-width {
+    width: 200px;
+  }
 </style>
