0be8c9fd2a84922092d6cc9111b24f9638120329
henrenx <henrenx@outlook.com>
2019-12-31 14:49:35 +0800

    加入了element

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import Video from 'video.js'
-import 'video.js/dist/video-js.css'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import Video from 'video.js';
+import 'video.js/dist/video-js.css';
+import 'element-ui/lib/theme-chalk/index.css';
 import {store} from './store';
 import VueResource from 'vue-resource';
-import VCharts from 'v-charts'
+import VCharts from 'v-charts';
 import axios from 'axios';
-import CxltToastr from 'cxlt-vue2-toastr'
+import CxltToastr from 'cxlt-vue2-toastr';
+import ElementUI from 'element-ui';
 
-Vue.use(CxltToastr)
+Vue.use(ElementUI);
+Vue.use(CxltToastr);
 Vue.use(VCharts);
 Vue.use(VueResource);
 Vue.http.options.root = 'https://dk2nj7uhfi.execute-api.us-east-2.amazonaws.com/prod/studentinfo';
